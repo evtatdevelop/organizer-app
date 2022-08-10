9f31f3252383188751646ddd41534ba39d23e6ec
Evgenii <evtatdevelop@gmail.com>
2022-08-10 16:41:04 +0700

    calculator common funcs and styles

--- src/features/calculator/calculater.js
+++ src/features/calculator/calculater.js
@@ -1,44 +1,92 @@
 import React from "react";
 import styles from './calculater.module.scss';
 import { useSelector, useDispatch } from "react-redux";
-import { calcDisplay, calcLeft, calcTop, setDisplay, setLeft, setTop } from "./calculatorSlice";
+import { calcDisplay, calcLeft, calcTop, setDisplay, setLeft, setTop, btnClick, getExp, getRes, inputExp, getLabel, back} from "./calculatorSlice";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDeleteLeft } from '@fortawesome/free-solid-svg-icons'
 
 export const Calculator = () => {
   const display = useSelector(calcDisplay)
   const left = useSelector(calcLeft)
   const top = useSelector(calcTop)
+  const exp = useSelector(getExp)
+  const res = useSelector(getRes)
+  const label = useSelector(getLabel)
   const dispatch = useDispatch();
 
+
   const moving = (e) => {
-    e.preventDefault();
-    let shiftX = e.clientX - e.target.getBoundingClientRect().left;
-    let shiftY = e.clientY - e.target.getBoundingClientRect().top;
+    const target = e.target.closest('section');
+    // e.preventDefault();
+    let shiftX = e.clientX - target.getBoundingClientRect().left;
+    let shiftY = e.clientY - target.getBoundingClientRect().top;
+   
     const move = (x, y) => {
       dispatch(setLeft(x - shiftX))
       dispatch(setTop(y - shiftY))
     }
     const mouseMove = e => move(e.pageX, e.pageY);
     document.addEventListener('mousemove', mouseMove)
-    e.target.ondragstart = () => false;
-    e.target.onmouseup = () => {
+    target.ondragstart = () => false;
+    target.onmouseup = () => {
       document.removeEventListener('mousemove', mouseMove);
       e.onMouseUp = null;
     };
   } 
 
-
+  const keyClick = e => dispatch(btnClick(e.target.value));
 
   const style = {left: `${left}px`, top: `${top}px`, display }
   return (
-    <section 
-      className={styles.calculator} 
+    <section className={styles.calculator} 
       style={style}
       onMouseDown={ e=>moving(e) }
     >
+      <div className={styles.display}>
+        <header>
+          <label>{label}</label>
+          <button onClick={() => dispatch(setDisplay('none'))}>&times;</button>
+        </header>
+        <input type='text' className={styles.main} readOnly
+          defaultValue={exp} 
+          // onInput={ e => dispatch(inputExp(e.target.value)) }
+        />
+        <input type='text'  className={styles.service} defaultValue={res} readOnly/>
+        <nav><ul>
+          <li><button
+            onClick={()=>dispatch(back())}
+          ><FontAwesomeIcon icon={faDeleteLeft}/></button></li>
+        </ul></nav>
+      </div>      
       
-      <button type='button' className={styles.onoff}
-        onClick={() => dispatch(setDisplay('none'))}
-      >on/off</button>
+      <div className={styles.keyboard} onClick={(e)=>keyClick(e)}>
+        <button value='set' className={`${styles.action} ${styles.btnText}`}>Set</button>
+        <button value='clear' className={`${styles.action} ${styles.btnText}`}>C</button>
+        <button value='brackets' className={`${styles.action} ${styles.btnText}`}>()</button>
+        <button value='divide' className={styles.action}>&divide;</button>
+
+        <button value={7}>7</button>
+        <button value={8}>8</button>
+        <button value={9}>9</button>
+        <button value='multiply' className={styles.action}>&times;</button>
+
+        <button value={4}>4</button>
+        <button value={5}>5</button>
+        <button value={6}>6</button>
+        <button value='subtract' className={styles.action}>&ndash;</button>
+
+        <button value={1}>1</button>
+        <button value={2}>2</button>
+        <button value={3}>3</button>
+        <button value='add' className={styles.action}>+</button>
+
+        <button value='sign'>&plusmn;</button>
+        <button value={0}>0</button>
+        <button value='dot'>.</button>
+        <button value='equals' className={styles.equals}>=</button>
+
+      </div>      
+
     </section>
   )
 }
--- src/features/calculator/calculatorSlice.js
+++ src/features/calculator/calculatorSlice.js
@@ -1,9 +1,12 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  display:  'grid',
-  left:     1,
-  top:      1,
+  display:  'flex',
+  left:     100,
+  top:      100,
+  exp:      '',
+  res:      '',
+  label:    'Calculator',
 }
 
 
@@ -14,15 +17,39 @@ export const calculatorSlice = createSlice({
     setDisplay: ( state, action ) => { state.display = action.payload },
     setLeft:    ( state, action ) => { state.left = action.payload },
     setTop:     ( state, action ) => { state.top = action.payload },
+    btnClick:   ( state, action ) => { 
+      switch ( action.payload ) {
+        case 'set': state.exp += action.payload; break;
+        case 'clear': state.exp = ''; break;
+        case 'brackets': state.exp += '()'; break;
+        case 'divide': state.exp += '/'; break;
+        case 'multiply': state.exp += '*'; break;
+        case 'subtract': state.exp += '-'; break;
+        case 'add': state.exp += '+'; break;
+        case 'sign': state.exp += Number(state.exp) * -1; break;
+        case 'dot': state.exp += '.'; break;
+        case 'equals': console.log(state.exp); break;
+        default: state.exp += action.payload;
+      };
 
+      state.res = action.payload;
+    },
+    inputExp:   ( state, action ) => { state.exp = action.payload },
+
+    back:       ( state, action ) => { 
+      state.exp = state.exp.substring(0, state.exp.length - 1); 
+    },
   },
 
 });
 
-export const { setDisplay, setLeft, setTop, } = calculatorSlice.actions;
+export const { setDisplay, setLeft, setTop, btnClick, inputExp, back} = calculatorSlice.actions;
 
 export const calcDisplay  = ( state ) => state.calculator.display;
 export const calcLeft     = ( state ) => state.calculator.left;
 export const calcTop      = ( state ) => state.calculator.top;
+export const getExp       = ( state ) => state.calculator.exp;
+export const getRes       = ( state ) => state.calculator.res;
+export const getLabel     = ( state ) => state.calculator.label;
 
 export default calculatorSlice.reducer;
